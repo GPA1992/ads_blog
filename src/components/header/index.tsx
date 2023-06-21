3ace6d8f0ab5defe60e2e56be0e38e6963020390
GPA <gabrielproencaaraujo@gmail.com>
2023-06-20 21:56:25 -0300

    material configurado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import './header.styles.sass';
+import { Tab, Tabs, Typography } from '@mui/material';
+
 
 export default function HeaderComponent() {
     return (
         <div className='header'>
-            <p>Header Component</p>
+            <Typography>Tenerifes</Typography>
+            <Tabs aria-label="basic tabs example">
+                <Tab label="Nossa História" />
+                <Tab label="Portfólio" />
+                <Tab label="A comunicação empresarial" />
+                <Tab label="Fale Conosco" />
+                <Tab label="Notícia" />
+            </Tabs>
         </div>
     );
 }
---
+++ src/components/ourHistory/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import styles from './ourStory.styles.sass';
+
+
+export default function OurStory() {
+    return (
+        <div>
+            <p>Main Article</p>
+        </div>
+    );
+}
